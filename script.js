701bfe2eb68b82384c24913058d96f2f864beff1
Thomas Perez <thomasperez@uagateway.org>
2016-12-07 21:16:50 +0000

    changed script in jsgrid javascript

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //Write your function calls below this line
+setColor(0,0,"orange");
+setColor(4,1,"black");
+setColor(5,2,"black");
+setColor(9,0,"orange");
+setColor(0,9,"orange");
+setColor(9,9,"orange");
+setColor(6,3,"black");
+setColor(6,4,"black");
+setColor(6,5,"black");
+setColor(6,6,"black");
+setColor(5,7,"black");
+setColor(4,8,"black");
+setColor(1,3,"red");
+setColor(2,3,"red");
+setColor(3,3,"red");
+setColor(1,6,"black");
+setColor(2,6,"black");
+setColor(3,6,"black");
+
+//for(var x = 0; x < 10; x += 1)
+//{
+//    setColor(x,x,"orange");
+//}
+// for x in range(0,10,2)
